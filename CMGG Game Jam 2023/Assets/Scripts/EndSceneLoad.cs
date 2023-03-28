93238a6b90a41bd314e9aabc0e13d898065910f7
Elizabeth Sun <elizabeth.sun@berkeley.edu>
2023-03-28 11:21:47 -0700

    memory scene change

--- CMGG Game Jam 2023/Assets/Scripts/EndSceneLoad.cs
+++ CMGG Game Jam 2023/Assets/Scripts/EndSceneLoad.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 //conditional, if 5 memories in inventory and step to certain location, end scene loads
 public class EndSceneLoad : MonoBehaviour
 {
-	if (Inventory.memories = 5)
+	if (Inventory.memories == 5)
 	{
 		SceneManager.LoadScene("End Scene");
 		Debug.Log("Scene Found");
